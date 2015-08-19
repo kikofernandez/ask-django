2a2b18d9dbe17e0b326ba340027a1297cfaf4ae3
Kiko Fernandez Reyes <kiko.fernandez@it.uu.se>
2015-08-19 12:42:25 +0200

    update serializer and __unicode__
    update serializer to display the name of the person requesting the
    service. in order to do that, i had to update the __unicode__ method
    in the corresponding model.

--- ask/instance/models.py
+++ ask/instance/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 class Skill(models.Model):
     tag = models.CharField(max_length=100, unique=True)
 
+    def __unicode__(self):
+        return u'%s' % (self.tag)
+
 class Service(models.Model):
     request = models.TextField()
     user_request = models.ForeignKey(User, related_name='author')
--- ask/instance/serializers.py
+++ ask/instance/serializers.py
@@ -2,6 +2,9 @@ from .models import Service, Skill
 from rest_framework import serializers
 
 class ServiceSerializer(serializers.ModelSerializer):
+    user_request = serializers.StringRelatedField()
+    skills = serializers.StringRelatedField(many=True)
+
     class Meta:
         model = Service
         fields = ('id', 'request', 'user_request', 'assigned', 'skills')
