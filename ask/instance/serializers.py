d80dba67d680b70f916c11bb3c9e91236150e377
Kiko Fernandez Reyes <kiko.fernandez@it.uu.se>
2015-08-20 16:18:47 +0200

    update serializer with more data

--- ask/instance/serializers.py
+++ ask/instance/serializers.py
@@ -1,13 +1,25 @@
 from .models import Service, Skill
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 class ServiceSerializer(serializers.ModelSerializer):
     user_request = serializers.StringRelatedField()
     skills = serializers.StringRelatedField(many=True)
+    state = serializers.SerializerMethodField()
+    priority = serializers.SerializerMethodField()
+
+    def get_state(self, obj):
+        return obj.get_state_display()
+
+    def get_priority(self, obj):
+        return obj.get_priority_display()
+
 
     class Meta:
         model = Service
-        fields = ('id', 'request', 'user_request', 'assigned', 'skills')
+        fields = ('id', 'request', 'user_request', 'assigned', 'skills', 'state', 'priority', 'no_people', 'cancellable')
+
+
 
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
